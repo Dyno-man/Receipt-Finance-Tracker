8b67aa99a78739da83970ab4e126a81a5dbf7c27
Dino <130256998+Dyno-man@users.noreply.github.com>
2026-01-21 15:47:08 -0500

    Added a couple data pulling functinos to the psqlTester script so that we can grab data easier

--- psqlTester.py
+++ psqlTester.py
@@ -2,6 +2,7 @@ import psycopg
 from datetime import date
 from dotenv import load_dotenv 
 import os
+import random
 
 load_dotenv()
 
@@ -17,32 +18,37 @@ TABLE = 'expenses'
 
 conninfo = f'postgresql://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DB}'
 
-try:
-    with psycopg.connect(conninfo) as conn:
-        print("DJ KHALID")
-        with conn.cursor() as cur:
-            cur.execute("SELECT version();")
-            print(cur.fetchone())
-except Exception as e:
-    print(f"Connection failed : {e}")
+# try:
+#     with psycopg.connect(conninfo) as conn:
+#         print("DJ KHALID")
+#         with conn.cursor() as cur:
+#             cur.execute("SELECT version();")
+#             print(cur.fetchone())
+# except Exception as e:
+#     print(f"Connection failed : {e}")
 
 
-print(date.today())
+# print(date.today())
 
-try:
-    with psycopg.connect(conninfo) as conn:
-        print("We're in")
+# try:
+#     with psycopg.connect(conninfo) as conn:
+#         print("We're in")
 
-        with conn.cursor() as cur:
+#         with conn.cursor() as cur:
 
-            cur.execute("INSERT INTO expenses (Date, ToE, Amount) VALUES (%s, %s, %s)", (date.today(), "Grocery", 100))
+#             cur.execute("INSERT INTO expenses (Date, ToE, Amount) VALUES (%s, %s, %s)", (date.today(), "Grocery", 100))
 
-            cur.execute("SELECT * FROM expenses;")
-except Exception as e:
-    print(f'Failed: {e}')
+#             cur.execute("SELECT * FROM expenses;")
+# except Exception as e:
+#     print(f'Failed: {e}')
 
-
-def insert_receipt(expense, cost, date=date.today()):
+"""
+This function does exactly what it says, it will insert receipt data into the psql db
+expense => This is expense type
+cost => This is the total cost
+date => This is the date and time of when the receipt was uploaded
+"""
+def insert_receipt(expense: str, cost: int, date=date.today()):
     try:
         with psycopg.connect(conninfo) as conn:
             with conn.cursor() as cur:
@@ -52,7 +58,11 @@ def insert_receipt(expense, cost, date=date.today()):
         print(f"Failed to insert: {e}")
         return False
 
-"""Make sure to pass in as a list, this is just in case use passes in multiple we are prepared for that"""
+
+"""
+This functino does exactly what it says, it will remove receipt data in the psql db
+ids => This is a list of ids that we want to remove
+"""
 def delete_receipt(ids: list):
     try:
         with psycopg.connect(conninfo) as conn:
@@ -65,3 +75,74 @@ def delete_receipt(ids: list):
         return False
     
 delete_receipt([1,3])
+
+
+"""
+Want to make a general purpose query for sorting, so I guess that means i want 4 functions.
+I will be sorting by Date, Type of Expense, and Amount
+I think sorting by IDs won't really be useful for a frontend scenario so this is how i want to do it for now
+"""
+
+"""
+For functions with the sort var you have three options for passing values
+-1 => is data < data
+0 => is data = data
+1 => is data > data
+"""
+
+def search_date(date: date, sort: int) -> bool:
+    try:
+        with psycopg.connect(conninfo) as conn:
+            with conn.cursor() as cur:
+                if sort == -1:
+                    cur.execute(f'SELECT date, toe, amount FROM {TABLE} WHERE date < {date}')    
+                elif sort == 0:
+                    cur.execute(f'SELECT date, toe, amount FROM {TABLE} WHERE date = {date}')
+                else:
+                    cur.execute(f'SELECT date, toe, amount FROM {TABLE} WHERE date > {date}')
+                return True
+    except Exception as e:
+        print(f'Failed to remove row: {e}')
+        return False
+
+def search_toe(toe: int) -> bool:
+    try:
+        with psycopg.connect(conninfo) as conn:
+            with conn.cursor() as cur:
+                cur.execute(f'SELECT date, toe, amount FROM {TABLE} WHERE toe = {toe}')
+                return True
+    except Exception as e:
+        print(f'Failed to remove row: {e}')
+        return False -1
+
+def search_amount(amount: int, sort: int) -> bool:
+    try:
+        with psycopg.connect(conninfo) as conn:
+            with conn.cursor() as cur:
+                if sort == -1:
+                    cur.execute(f'SELECT date, toe, amount FROM {TABLE} WHERE amount < {amount}')    
+                elif sort == 0:
+                    cur.execute(f'SELECT date, toe, amount FROM {TABLE} WHERE amount = {amount}')
+                else:
+                    cur.execute(f'SELECT date, toe, amount FROM {TABLE} WHERE amount > {amount}')
+                
+                for record in cur:
+                    print(record)
+                return True
+
+    except Exception as e:
+        print(f'Failed to remove row: {e}')
+        return False
+
+
+def insert_random_data():
+    categories = ["Grocery", "Automobile", "Other", "Wasteful"]
+    information = []
+
+    for _ in range(random.randint(10, 50)):
+        information.append([date.today(), categories[random.randint(0,3)], random.randint(1, 1000)])
+     
+    for item in information:
+        insert_receipt(item[1], item[2], item[0])
+
+search_amount(500, -1)
